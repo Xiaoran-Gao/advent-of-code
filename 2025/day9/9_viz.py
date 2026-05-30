8b0ce9818729cbac190fba660aa0c63ad2e9e10e
Xiaoran-Gao <xiaoran_gao@foxmail.com>
2026-05-30 16:00:03 +0800

    AoC 2025: Replace Day 9 Jupyter notebook with Python script and visualization output.

---
+++ 2025/day9/9_viz.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+
+with open("./input.txt", "r") as f:
+    coords = [list(map(int, c.split(','))) for c in f.read().splitlines()]
+
+plt.figure(figsize=(15, 15))
+
+x = [p[0] for p in coords] + [coords[0][0]]
+y = [p[1] for p in coords] + [coords[0][1]]
+
+plt.plot(x, y)
+plt.axis('equal')
+
+plt.savefig("9_viz.png", bbox_inches="tight")
+plt.show()
